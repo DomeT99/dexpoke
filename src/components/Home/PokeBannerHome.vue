6b0a61403052193c53cabf1d2594966f53252dff
DomeT99 <domenicotenace99@gmail.com>
2022-07-19 15:58:30 +0200

    Add Pokemon Fonts and style home banner

---
+++ src/components/Home/PokeBannerHome.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1 class="poke-solid f-size-64 title-dexpoke-style">Dexpoke</h1>
+    <hr class="opacity-0" />
+    <div class="column">
+        <p class="f-size-14 poke-classic">The Pokémon Web App suitable for everyone!</p>
+    </div>
+</template>
+<style lang="scss">
+.opacity-0 {
+    opacity: 0;
+}
+
+.title-dexpoke-style {
+    background: linear-gradient(305deg, #d5a100 40%, #0a285f 60%);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+}
+</style>
--- src/views/HomeViews.vue
+++ src/views/HomeViews.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import PokeBannerHomeVue from '../components/Home/PokeBannerHome.vue';
+</script>
 
 <template>
-
+    <section class="container has-text-centered">
+        <div class="column">
+            <PokeBannerHomeVue />
+        </div>
+    </section>
 </template>
